762f351bc316b21f5007c80614a807b991156988
kachi23-web <kaosykachi@gmail.com>
2024-10-01 03:27:39 +0100

    app.module fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { BlockchainModule } from './modules/blockchain/blockchain.module';
-import { TransactionsModule } from './modules/transactions/transactions.module';
-import { WalletsModule } from './modules/wallets/wallets.module';
-import { ContractModule } from './modules/contract/contract.module';
-import { ProvidersModule } from './shared/providers/providers.module';
-import { UtilsModule } from './shared/utils/utils.module';
-import { EnvironmentDevTsModule } from './environment/environment.dev.ts/environment.dev.ts.module';
-import { EnvironmentProdTsModule } from './environment/environment.prod.ts/environment.prod.ts.module';
-import { EnvironmentDevTsService } from './environment/environment.dev.ts/environment.dev.ts.service';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule, BlockchainModule, TransactionsModule, WalletsModule, ContractModule, ProvidersModule, UtilsModule, EnvironmentDevTsModule, EnvironmentProdTsModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, EnvironmentDevTsService],
+  providers: [AppService],
 })
 export class AppModule {}
