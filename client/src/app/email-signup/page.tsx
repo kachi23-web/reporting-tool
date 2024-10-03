6972a7dee076cecffbb40fa2b06ab629af6fdc95
kachi23-web <kaosykachi@gmail.com>
2024-10-03 13:33:37 +0100

    email

---
+++ client/src/app/email-signup/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import BVNDetails from '@/components/BVNDetails'
+import React, { useState } from 'react'
+
+
+export default function EmailSignupPage() {
+    const [toggle, setToggle] = useState(false)
+    function handleClick() {
+        setToggle(true)
+    } 
+  return (
+    <>
+    {
+    toggle ? <BVNDetails />
+    :
+    <div className="min-h-screen flex items-center">
+        <div className='container px-4 mx-auto'>
+            <form className="max-w-sm mx-auto">
+                <div className="mb-5">
+                    <label htmlFor="nin-number" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter Your BVN number</label>
+                    <input type="number" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+            
+            {/* Buttons */}
+                <div className='space-y-3'>
+                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleClick}>Submit</button>
+
+                        <button type="submit" className="text-black bg-transparent hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Back</button>
+                </div>
+            </form>
+
+        </div>
+    </div> 
+
+    }
+    </>
+  )
+}
--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -12,10 +12,13 @@ export default function SignupPage() {
                 <Link href="/nin-signup"  className="w-3/4">
                   <button type="button" className="w-full text-gray-900 bg-gray-100 hover:bg-white border-gray-300 focus:outline-none  focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Sign up with NIN</button>
                 </Link>
+                <Link href="/email-signup"  className="w-3/4">
+                  <button type="button" className="w-full text-gray-900 bg-gray-100 hover:bg-white border-gray-300 focus:outline-none  focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Sign up with Email</button>
+                </Link>
             </div>
 
             <div className='text-center my-10'>
-            <p>Don't have an account? <Link href="/signup" className='text-blue-500'>Sign up</Link></p>
+            <p>Don't have an account? <Link href="/signup" className='text-blue-500'>Login</Link></p>
         </div>
         </div>
     </div>
